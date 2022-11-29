5fd5b0da019073077c9be35827c08d7d48a3c948
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-11-28 18:07:53 -0800

    find loop in linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,31 +1,27 @@
-#include lists.h
+#include "lists.h"
 
 /**
- * check_cycle - check loop
- * @list: list
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to list
  *
- * Return: int
- */
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list, *fast = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	while (slow && fast && fast->next)
+	if (!list)
+		return (0);
+
+	while(slow && fast->next && fast->next->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
 
-		if (slow == fast)
-		{
-			slow = list;
-			while (slow != fast)
-			{
-				slow = slow->next;
-				fast = fast->next;
-			}
+		if (fast == slow)
 			return (1);
-		}
 	}
-
 	return (0);
 }
