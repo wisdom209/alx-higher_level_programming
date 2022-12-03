bba20bcf8de58ecc3895efbc79faa27bda18f11a
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-12-03 07:09:20 +0100

    Write a function that retrieves an element from a list like in C.

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    def replace_in_list(my_list, idx, element):
-        if (idx >= len(my_list) or idx < 0):
-            return my_list
-        my_list[idx] = element
+def replace_in_list(my_list, idx, element):
+    if (idx >= len(my_list) or idx < 0):
         return my_list
+    my_list[idx] = element
+    return my_list
