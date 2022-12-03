6c0083053c5402838b9bab9b83ff892311e09b93
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-12-03 08:43:59 +0100

    Write a function that retrieves an element from a list like in C.

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    def element_at(my_list, idx):
-        if (idx < 0):
-            return None
-        if (idx >= len(my_list)):
-            return None
-        return (my_list[idx])
+def element_at(my_list, idx):
+    if (idx < 0):
+        return None
+    if (idx >= len(my_list)):
+        return None
+    return (my_list[idx])
