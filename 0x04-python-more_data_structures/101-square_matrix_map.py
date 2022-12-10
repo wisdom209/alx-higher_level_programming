36474057f927ee6b0e0c6a50be0c249152d2119a
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-12-10 23:55:38 +0100

    square list of matrix elements

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda inner_list: [x**2 for x in inner_list], matrix))
+    return list(map(lambda a_list: list(map(lambda x: x**2, a_list)), matrix))
