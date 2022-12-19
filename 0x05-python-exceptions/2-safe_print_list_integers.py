3fd0fd4e797c63af788f37f1c81e9e251308918a
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-12-19 20:16:00 +0100

    update

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    """_summary_
-
-    Args:
-        my_list (list, optional): print only integers in list. Defaults to [].
-        x (int, optional): Length to print. Defaults to 0.
-
-    Returns:
-        int: Number of integers printed
-    """    
     num_printed = 0
     for i in range(x):
         try:
