0cd0d5815da8e9e7e93ecc775adf57702747dc2e
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-12-19 22:02:47 +0100

    update

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
+    """divide values at corresponding indices and return new list
+
+    Args:
+        my_list_1 (list): First List
+        my_list_2 (list): Second List
+        list_length (int): Length of new list
+
+    Returns:
+        list: New list from division of elements of two lists
+    """
     new_list = []
     for i in range(list_length):
         try:
