5eaa854b7a85c40205f273d89d4a4e887ac2440e
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-12-19 22:10:33 +0100

    update

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
+    """Raise an exception with a message
+
+    Args:
+        message (str, optional): Error message. Defaults to "".
+
+    Raises:
+        NameError: Exception raised
+    """    
     raise NameError(message)
