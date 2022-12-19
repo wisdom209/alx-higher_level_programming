64be3c756510d245ba4950900ec3dbe2d7471c2c
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-12-19 20:44:24 +0100

    update

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,9 +14,20 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             print("wrong type")
             new_list.append(0)
-        except Exception:
-            new_list.append(0)
         finally:
             continue
 
     return new_list
+
+
+my_l_1 = [10, 8, 4]
+my_l_2 = [2, 4, 4]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
+
+print("--")
+
+my_l_1 = [10, 8, 4, 4]
+my_l_2 = [2, 0, "H", 2, 7]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
