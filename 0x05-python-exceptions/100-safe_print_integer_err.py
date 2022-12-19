3ac2e1050d82d75101e3eb93f66829598649e809
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-12-19 21:20:27 +0100

    update

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,6 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as err:
-        print(err)
+        err = err.args[0]
+        sys.stderr.write("Exception: " + err + "\n")
         return False
