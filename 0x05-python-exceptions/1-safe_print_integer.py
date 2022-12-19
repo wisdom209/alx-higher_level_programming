8144ca0f00182491a438c9bf20d3d8fc0f16cdd1
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-12-19 19:31:33 +0100

    update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
+    """Print only integer values
+
+    Args:
+        value (any): Value to print
+
+    Returns:
+        Boolean: True if value is an integer
+    """
     try:
         print("{:d}".format(value))
     except ValueError as err:
