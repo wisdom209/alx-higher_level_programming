5105059070dd608c893bc7a5481daa409b9df4ed
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-12-19 19:07:27 +0100

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             if (i == x - 1):
                 print()
             num_printed += 1
-    except:
+    except IndexError:
         print()
     return num_printed
