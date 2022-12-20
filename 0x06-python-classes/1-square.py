e160daecf2ac8b12c4e47bfcea8dafba31be183c
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-12-20 17:23:00 +0100

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+_summary_
+
+"""
+
+
+class Square:
+    """Square class
+
+    private instance field : name
+
+    """
+
+    def __init__(self, size):
+        self.__size = size
