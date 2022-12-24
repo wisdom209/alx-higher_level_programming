dbe65a38665b97bee4c789bad0e16afc593b2951
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-12-24 15:29:16 +0100

    update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -30,18 +30,4 @@ def add_integer(a, b=98):
 
 
 if __name__ == '__main__':
-
     doctest.testfile("./tests/0-add_integer.txt")
-
-    print(add_integer(1, 2))
-    print(add_integer(100, -2))
-    print(add_integer(2))
-    print(add_integer(100.3, -2))
-    try:
-        print(add_integer(4, "School"))
-    except Exception as e:
-        print(e)
-    try:
-        print(add_integer(None))
-    except Exception as e:
-        print(e)
