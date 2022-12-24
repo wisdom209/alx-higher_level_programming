c21bd0e72effa636687a0aad296862eba7111244
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-12-24 23:02:39 +0100

    update

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,7 +5,15 @@ import doctest
 
 
 def print_square(size):
+    """prints a square
 
+    Args:
+        size (int): width of square
+
+    Raises:
+        TypeError: size must be an integer 
+        ValueError: size must be >= 0
+    """
     if (type(size) is float and size < 0):
         raise TypeError("size must be an integer")
 
