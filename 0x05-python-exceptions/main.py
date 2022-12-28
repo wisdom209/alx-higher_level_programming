10c954f2dbcb80265141f9c1c69a745b69ba143a
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2022-12-28 01:37:23 +0100

    update

--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -5,5 +5,5 @@ lib = ctypes.CDLL('./libPython.so')
 lib.print_python_list.argtypes = [ctypes.py_object]
 lib.print_python_bytes.argtypes = [ctypes.py_object]
 lib.print_python_float.argtypes = [ctypes.py_object]
-f = 9492307816406286.0
+f=1.41592653588893238462643383279502884197169399375105820974944592
 lib.print_python_float(f)
