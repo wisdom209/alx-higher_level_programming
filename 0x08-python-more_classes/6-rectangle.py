b0a599ec2404a620b4b57b800fcbfa47aa112cfc
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-01-03 02:24:02 +0100

    update

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -14,8 +14,8 @@ class Rectangle:
             width (int, optional): width. Defaults to 0.
             height (int, optional): height. Defaults to 0.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
