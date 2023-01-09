b9e6affab196dca088fddad4be9fe5c5a5236386
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-01-09 21:46:04 +0100

    update

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -29,4 +29,4 @@ class BaseGeometry:
         if (type(value) is not int):
             raise TypeError(f"{name} must be an integer")
         if (value < 1):
-            raise TypeError(f"{name} must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
