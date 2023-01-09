d2e282d38df3e1ce08d6db6493e3eb0200009fce
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-01-09 20:25:43 +0100

    update

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""My list class
+    """
+
+
+class MyList(list):
+    """Derived class of list
+
+    Args:
+            list (list): List Class
+    """
+
+    def __init__(self):
+        """initiates the class
+        """
+        super().__init__()
+
+    def print_sorted(self):
+        "prints the sorted list"
+        newlist = self[:]
+        newlist.sort()
+        print(newlist)
