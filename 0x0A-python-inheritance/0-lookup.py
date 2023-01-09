062d9fede4dc69f0873fe398bcf4b55af30048df
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-01-09 19:40:12 +0100

    update

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Prints methods of a class"""
+
+
+def lookup(obj):
+    """prints the attributes of an object
+
+    Args:
+            obj (any): object
+    """
+    dir(obj)
