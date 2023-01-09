e9c0681b01512bfe7f1c97e3ea9c37f4c7b1099c
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-01-09 21:19:03 +0100

    update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module implements a Geometry class
+    """
+
+
+class BaseGeometry:
+    """A Base Geometry class
+    """
+
+    def area(self):
+        """Base geometry area function
+
+            Raises:
+                    Exception: area() is not implemented
+            """
+        raise Exception("area() is not implemented")
