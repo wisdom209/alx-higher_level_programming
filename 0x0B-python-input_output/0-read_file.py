fb4b600e6fe89bb5c6b9034418bca60bde16ca24
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-01-11 02:53:01 +0100

    message

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,4 @@ def read_file(filename=""):
     """reads a file"""
     with open(filename, "r", encoding="UTF-8") as f:
         content = f.read()
-        if (content[-1] == '\n'):
-            content = content[:-1]
-        print(content)
+        print(content, end="")
