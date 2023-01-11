d18df9047903b6cdb4c201f8db365021a1af0ae0
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-01-11 03:15:27 +0100

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-"""I/0 module"""
+""" Module that contains a function that reads from a file """
 
 
 def read_file(filename=""):
-    """reads a file"""
-    with open(filename, "r", encoding="UTF-8") as f:
-        content = f.read()
-        print(content, end="")
+    """ Function that reads from a file
+
+    Args:
+        filename: filename
+
+    Raises
+        Exception: when the file can be opened
+
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
