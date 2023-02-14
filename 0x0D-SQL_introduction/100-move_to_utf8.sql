2f39441cb6c2251f50b0e40695239296dae22c9c
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-02-14 22:50:41 +0100

    change db and tabe to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,3 @@
+-- move to utf-8
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
