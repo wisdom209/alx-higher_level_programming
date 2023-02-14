1b8ffbb02247399d931b7149c32aa8c777b69574
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-02-14 21:42:55 +0100

    list and order by score descending

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- list scores greater than 10, order by highest score
-SELECT score, name FROM hbtn_0c_0.second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
 
