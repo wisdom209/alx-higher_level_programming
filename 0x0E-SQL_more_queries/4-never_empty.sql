2aa669aa5f691c7e3d9dc66fbefe4a9bfbed1b62
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-02-15 15:19:20 +0100

    create a table with default value

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- create a table
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
