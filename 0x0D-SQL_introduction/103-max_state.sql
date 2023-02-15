f25297f81ab66fe1203d53d714adf9c83882ef5a
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-02-15 01:05:00 +0100

    MAX and GROUP BY queries

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- script that displays the max temperature of each state (ordered by State name)
-SELECT state, MAX(value) as max_tmp FROM temperatures GROUP BY state ORDER BY state;
+SELECT state, MAX(value) as max_temp FROM temperatures GROUP BY state ORDER BY state;
