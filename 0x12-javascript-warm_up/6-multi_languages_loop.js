ad38aa213f386a104c38027efb6aa37c10a94711
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-14 02:48:35 +0000

    feat: Added 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const strArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const size = strArr.length;
+let i = 0;
+while (i < size) {
+  console.log(strArr[i]);
+  i++;
+}
