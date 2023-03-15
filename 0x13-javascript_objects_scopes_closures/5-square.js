9f21cbd3f373afb667074d6308302031898c5429
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-15 04:24:07 +0100

    feat: Added 5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+const Square = class extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+};
+
+module.exports = Square;
