0efeade8a67c4d04b1032dd7eeeda9b079e5fb06
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-18 01:45:24 +0100

    feat: Added 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Select States Module"""
+import sys
+import MySQLdb
+if __name__ == '__main__':
+    db_username = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+    db_host = "localhost"
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(user=db_username, password=db_password,
+                         host=db_host, database=db_name)
+    cursor = db.cursor()
+    sqlquery = f"SELECT * FROM states WHERE name=%s ORDER BY id ASC"
+    cursor.execute(sqlquery, (state_name_searched,))
+    rows = cursor.fetchall()
+    for i in rows:
+        print(i)
+    cursor.close()
+    db.close()
