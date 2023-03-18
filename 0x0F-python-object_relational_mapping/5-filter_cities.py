ada2b6fc9a4f6610a015baa753def97164e72d97
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-18 07:43:08 +0100

    feat: Added 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=db_username, password=db_password,
                          host=db_host, database=db_name)
     cursor = db.cursor()
-    sqlquery = "SELECT cities.name FROM cities INNER JOIN states ON\
+    sqlquery = "SELECT cities.name FROM cities JOIN states ON\
          cities.state_id = states.id WHERE states.name\
              LIKE BINARY %s ORDER BY cities.id ASC"
     cursor.execute(sqlquery, (state_name,))
