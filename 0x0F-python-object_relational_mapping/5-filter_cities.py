27a236ceba3e97c751231c86f267b3f6ee71ef83
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-18 07:34:47 +0100

    feat: Added 5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Select States Module"""
+import sys
+import MySQLdb
+if __name__ == '__main__':
+    db_username = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+    db_host = "localhost"
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(user=db_username, password=db_password,
+                         host=db_host, database=db_name)
+    cursor = db.cursor()
+    sqlquery = "SELECT cities.name FROM cities INNER JOIN states ON\
+         cities.state_id = states.id WHERE states.name\
+             LIKE BINARY %s ORDER BY cities.id ASC"
+    cursor.execute(sqlquery, (state_name,))
+    rows = cursor.fetchall()
+    for i in range(cursor.rowcount):
+        if (i == cursor.rowcount - 1):
+            print(rows[i][0])
+        else:
+            print(rows[i][0], end=", ")
+
+    cursor.close()
+    db.close()
