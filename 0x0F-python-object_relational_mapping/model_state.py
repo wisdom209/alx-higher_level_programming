aad4c76cde603ef8a2a1bbc4c359e473c8823b7d
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-18 11:28:56 +0100

    feat: Added model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """Creates a state table"""
 from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
-if __name__ == '__main__':
-    Base = declarative_base()
-
-    class State(Base):
-        """class representing state table"""
-        __tablename__ = 'states'
-        id = Column(Integer, autoincrement='auto',
-                    primary_key=True)
-        name = Column(String(128), nullable=False)
+Base = declarative_base()
+class State(Base):
+    """class representing state table"""
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement='auto',
+                primary_key=True)
+    name = Column(String(128), nullable=False)
