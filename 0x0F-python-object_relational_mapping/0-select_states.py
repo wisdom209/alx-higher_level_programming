8c4bb0ffca889ebe9558d93c4d7ff9fd13dbc911
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-18 01:10:16 +0100

    feat: Added 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys;
+import MySQLdb
+
+db_username = sys.argv[1]
+db_password = sys.argv[2]
+db_name = sys.argv[3]
+db_host = "localhost"
+
+db = MySQLdb.connect(user = db_username, password = db_password, host = db_host, database = db_name)
+
+cursor = db.cursor()
+
+cursor.execute("SELECT * FROM states")
+
+rows = cursor.fetchall()
+
+for i in rows:
+	print(i)
+cursor.close()
+db.close()
