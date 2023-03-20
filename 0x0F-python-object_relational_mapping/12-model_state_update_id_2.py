db049e18625d0ae54b61a8613859e143e2e2304e
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-20 03:38:33 +0100

    feat: Added 12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Fetches all the states"""
+"""adds a new state"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -7,8 +7,8 @@ from model_state import Base, State
 
 if __name__ == '__main__':
     """
-    Access to the database and get the states
-    from the database.
+    Access to the database and adds a
+    new state to the database.
     """
     db_username = sys.argv[1]
     db_pass = sys.argv[2]
@@ -17,9 +17,8 @@ if __name__ == '__main__':
     port = 3306
     engine = create_engine(
         f"mysql://{db_username}:{db_pass}@{host}:{port}/{db_name}")
-
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
-        print(f"{state.id}: {state.name}")  # getting table data
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = "New Mexico"
+    session.commit()
