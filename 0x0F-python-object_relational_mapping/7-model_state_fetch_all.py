a11c38c3159c58dc959b1fbb883d761052c22d44
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-20 02:32:29 +0100

    feat: Added 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,20 +2,24 @@
 """Fetches all the states"""
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == '__main__':
-	db_username = sys.argv[1]
-	db_pass = sys.argv[2]
-	db_name = sys.argv[3]
-	host = "localhost"
-	port = 3306
-	engine = engine = create_engine(
-		f"mysql://{db_username}:{db_pass}@{host}:{port}/{db_name}")
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_username = sys.argv[1]
+    db_pass = sys.argv[2]
+    db_name = sys.argv[3]
+    host = "localhost"
+    port = 3306
+    engine = create_engine(
+        f"mysql://{db_username}:{db_pass}@{host}:{port}/{db_name}")
 
-	Session = sessionmaker(bind=engine)
-	session = Session()
-	states = session.query(State).order_by(State.id).all()
-	for state in states:
-		print(f"{state.id}: {state.name}")  # getting table data
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")  # getting table data
