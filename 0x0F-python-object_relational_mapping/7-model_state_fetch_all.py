09c2d5bfb43b684475e35c938f1f04b16da4e84d
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-20 02:25:10 +0100

    feat: Added 7-model_state_fetch_all.py

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from model_state import Base, State
+
+
+db_username = sys.argv[1]
+db_pass = sys.argv[2]
+db_name = sys.argv[3]
+host = "localhost"
+port = 3306
+if __name__ == '__main__':
+    engine = engine = create_engine(
+        f"mysql://{db_username}:{db_pass}@{host}:{port}/{db_name}")
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")  # getting table data
