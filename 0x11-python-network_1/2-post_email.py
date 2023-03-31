b47c9c403677f6f2b253e3a7922aa66e1378df38
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-31 20:04:52 +0100

    Feat: Added 2-post-email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import urllib.request
 if __name__ == '__main__':
     """Makes a post request"""
     url = sys.argv[1]
-    data = {"email": f"{sys.argv[2]}"}
+    data = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(data).encode('ascii')
     request = urllib.request.Request
     with urllib.request.urlopen(request(url, data)) as response:
