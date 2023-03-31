becf62a0663ec9734c4e352fd27c59a613105eef
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-31 21:42:19 +0100

    Feat: Added 6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Post request with requests"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    """check post request"""
+    url = sys.argv[1]
+    email = sys.argv[2]
+    value = {"email": email}
+    response = requests.post(url, value)
+    print(response.text)
