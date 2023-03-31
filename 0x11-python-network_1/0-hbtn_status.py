841ad767dcd52d2d5e967edcbb509ef8fe68eea2
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-31 18:16:24 +0100

    Feat: Added 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module to implement GET with urllib"""
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+request = urllib.request.Request
+if __name__ == '__main__':
+    """run the code"""
+    with urllib.request.urlopen(request(url)) as response:
+        content = response.read()
+        print("Body response:")
+        print(f"	- type: {type(content)}")
+        print(f"	- content: {content}")
+        if 'utf-8' in response.info()['Content-Type']:
+            print(f"	- utf8 content: OK")
