d11931c6a8c8d6f7bb31a51f1736cb68619987a7
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-31 20:23:39 +0100

    Feat: Added 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """handle errors in http requests"""
-import urllib.request
-from urllib.error import HTTPError
-import sys
-url = sys.argv[1]
-request = urllib.request.Request
 if __name__ == '__main__':
     """handle the errors"""
+    import urllib.request
+    from urllib.error import HTTPError
+    import sys
+    url = sys.argv[1]
+    request = urllib.request.Request
     try:
         with urllib.request.urlopen(request(url)) as response:
             print(response.read().decode('utf-8'))
