e4a91e4313c05ee48f2b43c7f9b1b0f978dedfd2
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-31 19:01:17 +0100

    Feat: Added 1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""prints the X-Request-Id" header"""
+import sys
+import urllib.request
+
+if __name__ == '__main__':
+    """gets the header"""
+    url = sys.argv[1]
+    request = urllib.request.Request
+    with urllib.request.urlopen(request(url)) as response:
+        print(response.info().get('X-Request-Id'))
