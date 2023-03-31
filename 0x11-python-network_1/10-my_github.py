04791953cc9885b9ed2789b62166941575825202
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-04-01 00:00:04 +0100

    Feat: Added 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Get your github id"""
+import sys
+import requests
+if __name__ == '__main__':
+	user = sys.argv[1]
+	password = sys.argv[2]
+	url = "https://api.github.com/user"
+	response = requests.get(url, auth=(user, password))
+	print(response.json().get('id'))
