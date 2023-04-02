a7f2f8fa289063e7bfb1abe981ba1ac16c4a0369
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-04-02 15:02:46 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-""" This module contains """
+"""Get the 10 latest commits"""
+import requests
+import sys
 
 
-if __name__ == '__main__':
-    """Lists 10 commits from a github"""
-    import requests
-    import sys
+if __name__ == "__main__":
+    """Get the latest 10 commits"""
     repo = sys.argv[1]
     owner = sys.argv[2]
-    url = f'https://api.github.com/repos/{owner}/{repo}/commits?\
-    per_page=10'
-    res = requests.get(url)
-    # res.raise_for_status()
-    data = res.json()
-    for item in data:
-        sha = item.get('sha')
-        name = item.get('commit').get('author').get('name')
-        print(sha + ": " + name)
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits?\
+            author={owner}&sort=author-date&direction=desc&per_page=10"
+    response = requests.get(url)
+    json = response.json()
+    num = 0
+    for i in json:
+        sha = i.get('sha')
+        author = i.get("commit").get("author").get("name")
+        if (sha is not None):
+            print(f"{sha}: {author}")
+        num += 1
+        if (num == 10):
+            break
