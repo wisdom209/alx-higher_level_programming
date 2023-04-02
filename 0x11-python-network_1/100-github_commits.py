bba1e5e96bff98a9d69ecda1d64d095cd51801ec
Ononiwu Wisdom <60314035+wisdom209@users.noreply.github.com>
2023-04-02 08:44:29 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,37 +1,4 @@
 #!/usr/bin/python3
-"""
-This module contains script that uses the github api to display 10 commits
-from a github repository. The repo name and owner name are passed
-as command line arguments
-"""
-import requests
-from sys import argv
-
-
-def get_commits():
-    """
-    Lists 10 commits from a github repository using the github
-    API by : '<sha>: <author name>'
-    """
-    _, repo, owner = argv
-    url = f'https://api.github.com/repos/{owner}/{repo}/commits?per_page=10'
-    try:
-        res = requests.get(url)
-        res.raise_for_status()
-        data = res.json()
-        for item in data:
-            sha = f"{item.get('sha')}: "
-            name = f"{item.get('commit').get('author').get('name')}"
-            print(sha + name)
-    except requests.exceptions.HTTPError as err:
-        continue
-
-
-if __name__ == '__main__':
-    get_commits()
-
-"""
-#!/usr/bin/python3
 """Get the 10 latest commits"""
 import requests
 import sys
@@ -54,4 +21,3 @@ if __name__ == "__main__":
         num += 1
         if (num == 10):
             break
-"""
