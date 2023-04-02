0a37dfde8313c3e31ec610b718e26dcdc3921f1f
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-04-02 14:50:53 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -16,13 +16,10 @@ if __name__ == '__main__':
     owner = sys.argv[2]
     url = f'https://api.github.com/repos/{owner}/{repo}/commits?\
     per_page=10'
-    try:
-        res = requests.get(url)
-        # res.raise_for_status()
-        data = res.json()
-        for item in data:
-            sha = f"{item.get('sha')}: "
-            name = item.get('commit').get('author').get('name')
-            print(sha + name)
-    except Exception as e:
-        pass
+    res = requests.get(url)
+    # res.raise_for_status()
+    data = res.json()
+    for item in data:
+        sha = f"{item.get('sha')}: "
+        name = item.get('commit').get('author').get('name')
+        print(sha + name)
