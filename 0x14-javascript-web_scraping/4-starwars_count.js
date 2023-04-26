78dad7f29d7cbb351514d044b7843219dc7414a1
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-04-26 01:37:37 +0100

    Added 2-export_to_JSON.py

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,28 +2,29 @@
 /*  prints the number of movies where the character “Wedge Antilles” is present */
 
 const request = require('request');
-const BASEURL = 'https://swapi-api.alx-tools.com/api/';
+/* const BASEURL = 'https://swapi-api.alx-tools.com/api/'; */
 
 if (process.argv.length >= 3) {
   const filmApi = process.argv[2];
-  const wedgeApi = BASEURL + 'people/18/';
+  /* const wedgeApi = BASEURL + 'people/18/'; */
   let movieCount = 0;
 
   request(filmApi, (err, response, body) => {
-    if (err) throw err;
-    body = JSON.parse(body);
-    const results = body.results;
+    if (err == null) {
+      body = JSON.parse(body);
+      const results = body.results;
 
-    for (const result in results) {
-      const charactersApi = results[result].characters;
+      for (const result in results) {
+        const charactersApi = results[result].characters;
 
-      for (const character in charactersApi) {
-        if (charactersApi[character] === wedgeApi) {
-          movieCount++;
-          continue;
+        for (const character in charactersApi) {
+          if (charactersApi[character].search('18') > 0) {
+            movieCount++;
+            continue;
+          }
         }
       }
+      console.log(movieCount);
     }
-    if (movieCount > 0) { console.log(movieCount); }
   });
 }
