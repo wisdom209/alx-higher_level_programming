04ca3db4fbb8415ee88b12971c97c9b6b686b58b
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-04-26 01:39:01 +0100

    Added 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,6 @@
 
 const request = require('request');
 /* const BASEURL = 'https://swapi-api.alx-tools.com/api/'; */
-
 if (process.argv.length >= 3) {
   const filmApi = process.argv[2];
   /* const wedgeApi = BASEURL + 'people/18/'; */
